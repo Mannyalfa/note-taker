22c7c86b97e316b06b0e0c398457d276e0be76db
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-07-07 04:27:30 -0400

    added express server & port

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-const fs = require("fs");
+//required modules
+const path = require ('path')
 const express = require('express');
+
+//used to store and retrieve notes
+const dbJson = require('./db/db.json')
+
+//set up express server app @ port 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//add listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
+
