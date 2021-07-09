5eded7ee756c14e6620a87912fd3bcff3c3ea388
Manny Alfaro <78244552+Mannyalfa@users.noreply.github.com>
2021-07-09 14:01:36 -0400

    local host complete

---
+++ routes/apiRoutes.js
@@ -0,0 +1,2 @@
+const fs = require("fs");
+const noteData = require("../db/db.json");
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-//required modules
-const path = require ('path')
+const fs = require('fs');
+const path = require ('path');
 const express = require('express');
 
 //used to store and retrieve notes
-const dbJson = require('./db/db.json')
+const dbJson = require('./db/db.json');
 
 //set up express server app @ port 3001
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// reads url/json
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+/*js route files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);*/
+
+
 //add listener
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("App on port: " + PORT);
 });
 
